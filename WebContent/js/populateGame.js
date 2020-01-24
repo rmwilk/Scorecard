54c21b8967d312c3251dd556a3d3e59a03e1ae53
rmwilk <rwilk@umich.edu>
2020-01-23 21:43:14 -0500

    game scores working

--- WebContent/js/populateGame.js
+++ WebContent/js/populateGame.js
@@ -9,36 +9,48 @@ function clearTableData() {
 	testPopulate();
 }
 
-function testPopulateWithModals(){
+function putSpace(num) {
+	if(num <10) {
+		return "&nbsp;&nbsp;";
+	}
+	else {
+		return "";
+	}
+}
+
+function testPopulateWithModals() {
 	element("game-table").innerHTML += `
 		<tr>
 			<th id="hole#">#</th>
 			<th id="par">Par</th>
-			<th id="p1">P1</th>
-			<th id="p2">P2</th>
-			<th id="p3">P3</th>
-			<th id="p4">P4</th>
+			<th id="p1name">P1</th>
+			<th id="p2name">P2</th>
+			<th id="p3name">P3</th>
+			<th id="p4name">P4</th>	
 			<th></th>
-		</tr>`;
-	for(var i = 1; i <= 18; i++){
-		getID("game-table").innerHTML += `
+		</tr>
+		`;
+	for(var i = 1; i <= 18; i++) {
+		var space = putSpace(i);
+		element("game-table").innerHTML += `
 			<tr>
 				<td>
-					<button type="button" class="btn btn-primary"
-					data-toggle="modal"	data-target="#holeModal" onclick="buildHoleModal(`+i+`)">
-						Hole ` + i + `
-					</button></td>
-				<td>2</td>
-				<td id="p1hole` + i + `">
+					<button type="button" class="btn btn-primary" onclick="buildHoleScoreModal(`+i+`);"
+					data-toggle="modal"	data-target="#holeScoreModal">
+						Hole ` + space + i + `
+					</button>
+				</td>
+				<td id="hole`+i+`par" class="bold">2</td>
+				<td id="p1hole` + i + `" class="stronger">
 					-
 				</td>
-				<td id="p2hole` + i + `">
+				<td id="p2hole` + i + `" class="stronger">
 					-
 				</td>
-				<td id="p3hole` + i + `">
+				<td id="p3hole` + i + `" class="stronger">
 					-
 				</td>
-				<td  id="p4hole` + i + `">
+				<td  id="p4hole` + i + `" class="stronger">
 					-
 				</td>
 				<td>
@@ -47,7 +59,8 @@ function testPopulateWithModals(){
 						Hint
 					</button>
 				</td>
-			</tr>`;
+			</tr>
+			`;
 	}
 }
 
@@ -139,7 +152,7 @@ function buildHoleHintModal(hole){
 		</div>`;
 }
 
-function buildHoleModal(hole) {
+function buildHoleScoreModal(hole) {
 	element("holeScoreModalSpace").innerHTML = `
 	<div class="modal fade" id="holeScoreModal" tabindex="-1"
 		role="dialog" aria-labelledby="#holeScoreModalTitle"
@@ -157,57 +170,55 @@ function buildHoleModal(hole) {
 				</div>
 				<div class="modal-body">
 					<div class="container" style="text-align: center">
-						<h3>Input Everyone's Scores</h3>
-						<table class="table">
+						<h3>Input Everyone's Scores for Hole ` + hole + `</h3>
+						<table class="table table-condensed">
 							<thead>
 								<tr>
-									<th>P1</th>
-									<th>P2</th>
-									<th>P3</th>
-									<th>P4</th>									
+									<th id="p1nameLB">P1</th>
+									<th id="p2nameLB">P2</th>
+									<th id="p3nameLB">P3</th>
+									<th id="p4nameLB">P4</th>									
 								</tr>
 							</thead>
 							<tbody>
 								<tr>
-			<td>` + i + `</td>
-			<td>2</td>
-			<td id="p1h`+hole+`">
-				<select class="mdb-select md-form">
-					<option value="4" selected disabled hidden="true"></option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-				</select>
-			</td>
-			<td id="p2h`+hole+`">
-				<select class="mdb-select md-form">
-					<option value="4" selected disabled hidden="true"></option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-				</select>
-			</td>
-			<td id="p3h`+hole+`">
-				<select class="mdb-select md-form">
-					<option value="4" selected disabled hidden="true"></option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-				</select>
-			</td>
-			<td id="p4h`+hole+`">
-				<select class="mdb-select md-form">
-					<option value="4" selected disabled hidden="true"></option>
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-				</select>
-			</td>
-		</tr>
+									<td>
+										<select  id="p1h`+hole+`" class="mdb-select md-form">
+											<option value="4" selected disabled hidden="true"></option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+											<option value="3">3</option>
+											<option value="4">4</option>
+										</select>
+									</td>
+									<td>
+										<select  id="p2h`+hole+`" class="mdb-select md-form">
+											<option value="4" selected disabled hidden="true"></option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+											<option value="3">3</option>
+											<option value="4">4</option>
+										</select>
+									</td>
+									<td>
+										<select  id="p3h`+hole+`" class="mdb-select md-form">
+											<option value="4" selected disabled hidden="true"></option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+											<option value="3">3</option>
+											<option value="4">4</option>
+										</select>
+									</td>
+									<td>
+										<select  id="p4h`+hole+`" class="mdb-select md-form">
+											<option value="4" selected disabled hidden="true"></option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+											<option value="3">3</option>
+											<option value="4">4</option>
+										</select>
+									</td>
+								</tr>
 							</tbody>
 						</table>
 					</div>
@@ -217,7 +228,7 @@ function buildHoleModal(hole) {
 						Close
 					</button>
 					<button type="button" class="btn btn-primary" data-dismiss="modal" 
-					onclick="enterScores(hole);">
+					onclick="enterScores(`+hole+`);">
 						Enter
 					</button>
 				</div>
@@ -228,11 +239,105 @@ function buildHoleModal(hole) {
 }
 
 function enterScores(hole){
+	console.log(element("p1h"+hole).value);
 	element("p1hole"+hole).innerHTML = element("p1h"+hole).value;
 	element("p2hole"+hole).innerHTML = element("p2h"+hole).value;
 	element("p3hole"+hole).innerHTML = element("p3h"+hole).value;
 	element("p4hole"+hole).innerHTML = element("p4h"+hole).value;
 }
+
+function isNumber(num) {
+	switch (num) {
+	case 1:case 2: case 3: case 4: case 0: {
+		return true;		
+	}
+		default: {
+			return false;
+		}
+	}
+}
+
 function calculateScores() {
-	window.location = "./index.html";
+	var p1scores = 0;
+	var p2scores = 0;
+	var p3scores = 0;
+	var p4scores = 0;
+	var par = 0;
+	var p1name = element("p1name").innerHTML;
+	var p2name = element("p2name").innerHTML;
+	var p3name = element("p3name").innerHTML;
+	var p4name = element("p4name").innerHTML;
+	
+	for(var i = 1; i <= 18; i ++) {
+		if(isNumber(parseInt(element("p1hole" + i).innerHTML)) ) {
+			p1scores +=	parseInt(element("p1hole" + i).innerHTML);			
+		}
+		if(isNumber(parseInt(element("p2hole" + i).innerHTML)) ) {
+			p2scores +=	parseInt(element("p2hole" + i).innerHTML);			
+		}
+		if(isNumber(parseInt(element("p3hole" + i).innerHTML)) ){
+			p3scores +=	parseInt(element("p3hole" + i).innerHTML);			
+		}
+		if(isNumber(parseInt(element("p4hole" + i).innerHTML)) ){
+			p4scores +=	parseInt(element("p4hole" + i).innerHTML);			
+		}
+		par += parseInt(element("hole"+i+"par").innerHTML);
+	} // end for loop
+	
+	
+	element("leaderboardModalSpace").innerHTML = `
+		<div class="modal fade" id="leaderboardModal" tabindex="-1"
+			role="dialog" aria-labelledby="#leaderboardModalTitle"
+			aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h2 class="modal-title" id="leaderboardModalTitle">
+							Leaderboard
+						</h2>
+						<button type="button" class="close" data-dismiss="modal"
+							aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="container" style="text-align: center; align-content: center">
+							<h3>Lowest score wins!</h3>
+							<table id="leaderboard" class="table" style="margin: auto">
+								<thead>
+									<tr>
+										<th>Par</th>
+										<th>`+p1name+`</th>
+										<th>`+p2name+`</th>
+										<th>`+p3name+`</th>
+										<th>`+p4name+`</th>
+									</tr>
+								</thead>
+								<tbody>
+										<tr>
+											<td>`+par+`
+											<td>`+p1scores+`</td>
+											<td>`+p2scores+`</td>
+											<td>`+p3scores+`</td>
+											<td>`+p4scores+`</td>
+										</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" 
+						data-dismiss="modal">
+							Back to Scorecard
+						</button>
+						<button type="button" class="btn btn-success" 
+						data-dismiss="modal" onclick="testOpenIndexPage();">
+							Submit Scorecard
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		`;
+	
 }
